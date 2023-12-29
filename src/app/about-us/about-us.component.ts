ee2d75f2163fbe70b4eee7d1b79673fe0ecfa1ed
desaraballabani <desara.ballabani@base-com.al>
2023-12-29 23:03:47 +0100

    Update some style

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -14,17 +14,17 @@ export class AboutUsComponent implements OnInit {
   constructor() {
     this.images = [
       {
-        source: 'https://t3.ftcdn.net/jpg/01/36/25/58/360_F_136255856_XGPHT6zyNJay48Dprr0bTVYb4XlUuSaI.jpg',
+        source: 'https://t3.ftcdn.net/jpg/04/39/59/32/360_F_439593202_3ZYjuJk9ukG0zzWnNEaUxZKXF01Z0zWu.jpg',
         alt: 'Image 1',
         overlayText: '"Our mission is to provide seamless shopping experiences, catering to diverse needs while ensuring customer satisfaction and convenience."'
       },
       {
-        source: 'https://t3.ftcdn.net/jpg/01/36/25/58/360_F_136255856_XGPHT6zyNJay48Dprr0bTVYb4XlUuSaI.jpg',
+        source: 'https://t3.ftcdn.net/jpg/04/39/59/32/360_F_439593202_3ZYjuJk9ukG0zzWnNEaUxZKXF01Z0zWu.jpg',
         alt: 'Image 2',
         overlayText: '"Our vision is to become a market leader in delivering innovative products and services, setting new standards for excellence and reliability."'
       },
       {
-        source: 'https://t3.ftcdn.net/jpg/01/36/25/58/360_F_136255856_XGPHT6zyNJay48Dprr0bTVYb4XlUuSaI.jpg',
+        source: 'https://t3.ftcdn.net/jpg/04/39/59/32/360_F_439593202_3ZYjuJk9ukG0zzWnNEaUxZKXF01Z0zWu.jpg',
         alt: 'Image 3',
         overlayText: '"At our core, we uphold integrity, transparency, and a customer-centric approach, striving for excellence in every interaction and transaction."'
       }
@@ -35,5 +35,4 @@ export class AboutUsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
