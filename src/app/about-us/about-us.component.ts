14c4c1142a20266223f81501e428718a541553bf
desaraballabani <desara.ballabani@base-com.al>
2024-01-09 14:39:40 +0100

    Update some style

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -14,19 +14,19 @@ export class AboutUsComponent implements OnInit {
   constructor() {
     this.images = [
       {
-        source: 'https://i.pinimg.com/564x/b6/6f/50/b66f504ae848468a198b935b06240475.jpg',
+        source: 'https://i.pinimg.com/564x/29/b0/1e/29b01e5fa43840955c7153e6ca99ae2c.jpg',
         alt: 'Image 1',
-        overlayText: 'Our mission is to provide seamless shopping experiences, catering to diverse needs while ensuring customer satisfaction and convenience.'
+        overlayText: '"Our mission is to provide seamless shopping experiences, catering to diverse needs while ensuring customer satisfaction and convenience."'
       },
       {
-        source: 'https://i.pinimg.com/564x/b6/6f/50/b66f504ae848468a198b935b06240475.jpg',
+        source: 'https://i.pinimg.com/564x/29/b0/1e/29b01e5fa43840955c7153e6ca99ae2c.jpg',
         alt: 'Image 2',
-        overlayText: 'Our vision is to become a market leader in delivering innovative products and services, setting new standards for excellence and reliability.'
+        overlayText: '"Our vision is to become a market leader in delivering innovative products and services, setting new standards for excellence and reliability."'
       },
       {
-        source: 'https://i.pinimg.com/564x/b6/6f/50/b66f504ae848468a198b935b06240475.jpg',
+        source: 'https://i.pinimg.com/564x/29/b0/1e/29b01e5fa43840955c7153e6ca99ae2c.jpg',
         alt: 'Image 3',
-        overlayText: 'At our core, we uphold integrity, transparency, and a customer-centric approach, striving for excellence in every interaction and transaction.'
+        overlayText: '"At our core, we uphold integrity, transparency, and a customer-centric approach, striving for excellence in every interaction and transaction."'
       }
     ];
   }
