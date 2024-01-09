ea09b5ddf2327d0c4e7cd102512e0700ac0872e7
desaraballabani <desara.ballabani@base-com.al>
2024-01-09 12:23:46 +0100

    Update some style

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -14,19 +14,19 @@ export class AboutUsComponent implements OnInit {
   constructor() {
     this.images = [
       {
-        source: 'https://t3.ftcdn.net/jpg/04/39/59/32/360_F_439593202_3ZYjuJk9ukG0zzWnNEaUxZKXF01Z0zWu.jpg',
+        source: 'https://i.pinimg.com/564x/b6/6f/50/b66f504ae848468a198b935b06240475.jpg',
         alt: 'Image 1',
-        overlayText: '"Our mission is to provide seamless shopping experiences, catering to diverse needs while ensuring customer satisfaction and convenience."'
+        overlayText: 'Our mission is to provide seamless shopping experiences, catering to diverse needs while ensuring customer satisfaction and convenience.'
       },
       {
-        source: 'https://t3.ftcdn.net/jpg/04/39/59/32/360_F_439593202_3ZYjuJk9ukG0zzWnNEaUxZKXF01Z0zWu.jpg',
+        source: 'https://i.pinimg.com/564x/b6/6f/50/b66f504ae848468a198b935b06240475.jpg',
         alt: 'Image 2',
-        overlayText: '"Our vision is to become a market leader in delivering innovative products and services, setting new standards for excellence and reliability."'
+        overlayText: 'Our vision is to become a market leader in delivering innovative products and services, setting new standards for excellence and reliability.'
       },
       {
-        source: 'https://t3.ftcdn.net/jpg/04/39/59/32/360_F_439593202_3ZYjuJk9ukG0zzWnNEaUxZKXF01Z0zWu.jpg',
+        source: 'https://i.pinimg.com/564x/b6/6f/50/b66f504ae848468a198b935b06240475.jpg',
         alt: 'Image 3',
-        overlayText: '"At our core, we uphold integrity, transparency, and a customer-centric approach, striving for excellence in every interaction and transaction."'
+        overlayText: 'At our core, we uphold integrity, transparency, and a customer-centric approach, striving for excellence in every interaction and transaction.'
       }
     ];
   }
