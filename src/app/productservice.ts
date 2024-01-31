9511206e6c9f0361a8f941759da7a047fa51938f
desaraballabani <desara.ballabani@base-com.al>
2024-01-31 13:03:36 +0100

    Using firebase as a database

--- src/app/productservice.ts
+++ src/app/productservice.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { combineLatest, map, pipe, merge, Observable } from 'rxjs';
 import { CategoryService } from './service/category.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 import { zip } from 'rxjs';
 import { Product } from './product';
@@ -10,69 +12,75 @@ import { Product } from './product';
 export class ProductService {
   cartData = new EventEmitter<Product[] | []>();
   constructor(
+    private firestore: AngularFirestore,
     private http: HttpClient,
     private categoryService: CategoryService) { }
 
 
-  products$ = this.http
-    .get<{ [id: string]: Product }>(
-      `https://fakestoreapi.com/products`
-    )
-    .pipe(
-      map((products) => {
-        let productsData: Product[] = [];
-        for (let id in products) {
-          productsData.push({ ...products[id], id });
-        }
-        return productsData;
-      }),
+  products$ = this.firestore
+    .collection<Product>('products')
+    .valueChanges({ idField: 'id' });
 
-    );
-
-  // postsWithCategory$ = combineLatest([
-  //   this.posts$,
-  //   this.categoryService.categories$,
-  // ]).pipe(
-  //   map(([posts]) => {
-  //     return posts.map((post) => {
-  //       return {
-  //         ...post
-  //       } as Product;
-  //     });
-  //   }),
-
-  // );
-
-  // allPosts$ = merge(
-  //   this.postsWithCategory$,
-
-  // )
-
-
-  getAllProducts() {
-    return this.http.get<any>('https://fakestoreapi.com/products').pipe(
-      map((res: any) => {
-        return res;
-      })
-    );
+  getAllProducts(): Observable<Product[]> {
+    return this.products$;
   }
 
-  // New method to get a product by ID
   getProductById(productId: string): Observable<Product | undefined> {
-    return this.products$.pipe(
-      map((products: Product[]) => {
-        return products.find((product) => product.id === productId);
-      })
-    );
+    return this.firestore
+      .doc<Product>(`products/${productId}`)
+      .valueChanges({ idField: 'id' });
   }
-
 
   getAllCategories() {
-    return this.http.get('https://fakestoreapi.com/products/categories')
+    return this.firestore.collection('categories').valueChanges();
   }
 
   getProductsByCategory(keyword: string) {
-    return this.http.get('https://fakestoreapi.com/products/category/' + keyword)
+    return this.firestore
+      .collection<Product>('products', ref => ref.where('category', '==', keyword))
+      .valueChanges({ idField: 'id' });
   }
+
+
+  // products$ = this.http
+  //   .get<{ [id: string]: Product }>(
+  //     `https://fakestoreapi.com/products`
+  //   )
+  //   .pipe(
+  //     map((products) => {
+  //       let productsData: Product[] = [];
+  //       for (let id in products) {
+  //         productsData.push({ ...products[id], id });
+  //       }
+  //       return productsData;
+  //     }),
+
+  //   );
+
+  // getAllProducts() {
+  //   return this.http.get<any>('https://fakestoreapi.com/products').pipe(
+  //     map((res: any) => {
+  //       return res;
+  //     })
+  //   );
+  // }
+
+
+  // getProductById(productId: string): Observable<Product | undefined> {
+  //   return this.products$.pipe(
+  //     map((products: Product[]) => {
+  //       return products.find((product) => product.id === productId);
+  //     })
+  //   );
+  // }
+
+
+  // getAllCategories() {
+  //   return this.http.get('https://fakestoreapi.com/products/categories')
+  // }
+
+  // getProductsByCategory(keyword: string) {
+  //   return this.http.get('https://fakestoreapi.com/products/category/' + keyword)
+  // }
 
 }
